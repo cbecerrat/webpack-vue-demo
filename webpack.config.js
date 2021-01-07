fda295f327b786ce7eee752eb4ef5d25b2933472
César Octavio Becerra Torres <cesaroctavio.becerra.torres.next@bbva.com>
2021-01-07 15:09:53 -0600

    Changed to dev mode

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    mode: 'production',
+    mode: 'development',
     entry: './src/index.ts',
     devtool: 'inline-source-map',
     module: {
