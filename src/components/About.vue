a0c4a2748ea3c63ccf846414f8af85413e6f04e2
César Octavio Becerra Torres <cesaroctavio.becerra.torres.next@bbva.com>
2021-02-03 10:06:28 -0600

    Added missing files

---
+++ src/components/About.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="alert alert-primary" role="alert">
+        {{ message }}
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from "vue";
+
+    const About = defineComponent({
+        data() {
+            return {
+                message: 'ABOUT',
+            };
+        }
+    });
+
+    export default About;
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="alert alert-primary" role="alert">
+        {{ message }}
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from "vue";
+
+    const Home = defineComponent({
+        data() {
+            return {
+                message: 'HOME',
+            };
+        }
+    });
+
+    export default Home;
+</script>
