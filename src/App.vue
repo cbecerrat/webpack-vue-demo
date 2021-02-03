0577ce84490e81a3e711b227bded4063972c0bc8
César Octavio Becerra Torres <cesaroctavio.becerra.torres.next@bbva.com>
2021-02-02 19:43:44 -0600

    Added vue router

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,15 @@
     <div class="alert alert-primary" role="alert">
         This is the vue template... check it out!
     </div>
+
+    <p>
+        <!-- use the router-link component for navigation. -->
+        <!-- specify the link by passing the `to` prop. -->
+        <!-- `<router-link>` will render an `<a>` tag with the correct `href` attribute -->
+        <router-link to="/">Go to Home</router-link>
+        <router-link to="/about">Go to About</router-link>
+    </p>
+    <router-view></router-view>
     {{ counter }}
 </template>
 
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,22 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { createApp } from 'vue';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import App from './App.vue';
 
-createApp(App).mount('#app')
+import Home from './components/Home.vue';
+import About from './components/About.vue'
+
+const routes = [
+    { path: '/', component: Home },
+    { path: '/about', component: About }
+];
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app')
